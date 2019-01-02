3d71b6ca63f57450e6e731918ad21b20aed19e33
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-01-02 12:15:02 +0200

    Implemented bread builder funtionality

--- mobile/src/main/java/lekkersolutions/chrispieterse/bestpracticesanddesignpatterns/MainActivity.java
+++ mobile/src/main/java/lekkersolutions/chrispieterse/bestpracticesanddesignpatterns/MainActivity.java
@@ -1,6 +1,7 @@
 package lekkersolutions.chrispieterse.bestpracticesanddesignpatterns;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -16,12 +17,30 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.text_view);
 
         AbstractFactory filling_factory = FactoryGenerator.getFactory("FIL");
+        assert filling_factory != null;
         Filling filling = filling_factory.getFilling("CHE");
         Log.d(DEBUG_TAG, filling.name() + " : " + filling.calories());
 
         AbstractFactory bread_factory = FactoryGenerator.getFactory("BRE");
+        assert bread_factory != null;
         Bread bread = bread_factory .getBread("ROL");
         Log.d(DEBUG_TAG, bread.name() + " : " + bread.calories());
+
+        // Build a customized sandwich
+        SandwichBuilder builder = new SandwichBuilder();
+        Sandwich custom = new Sandwich();
+        // Simulate user selections
+        custom = builder.build(custom, new Roll());
+        custom = builder.build(custom, new Cheese());
+        Log.d(DEBUG_TAG, "CUSTOMIZED");
+        custom.getSandwich();
+        custom.getCalories();
+
+        // Build a ready made sandwich
+        Sandwich offTheShelf = SandwichBuilder.readyMade();
+        Log.d(DEBUG_TAG, "READY MADE");
+        offTheShelf.getSandwich();
+        offTheShelf.getCalories();
 
         textView.setText(new StringBuilder()
                             .append(bread.name())
